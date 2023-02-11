5307e30a120320a06afab16ff727aec52179ba45
Phạm Quyền <quyenph@ichiba.vn>
2023-02-11 09:25:37 +0700

    chore: branch using sub-project storybook

--- example/App.tsx
+++ example/App.tsx
@@ -1,9 +1,9 @@
-import StorybookUIRoot from "./storybook";
+// import StorybookUIRoot from "./storybook";
 
-// import App from "./src/App";
+import App from "./src/App";
 
 /**
  * export App if you are Developer
  * export StorybookUIRoot if you want to Build Apk or Ipa file
  */
-export { StorybookUIRoot as default };
+export { App as default };
